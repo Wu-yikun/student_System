622d99b98929b7df4459932d9f9f5a3ae54a163c
Wu-yikun <577159462@qq.com>
2021-03-11 11:25:15 +0800

    update

--- src/_422/studentsystem/dao/JdbcConfig.java
+++ src/_422/studentsystem/dao/JdbcConfig.java
@@ -2,7 +2,7 @@ package _422.studentsystem.dao;
 
 public interface JdbcConfig {
     String DRIVER = "com.mysql.jdbc.Driver";
-    String URL = "jdbc:mysql://localhost:3306/StudentSystemDao";
+    String URL = "jdbc:mysql://110.87.46.81:3306/StudentSystemDao";
     String USERNAME = "root";
     String PASSWORD = "123456";
 }
